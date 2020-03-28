f8375abc48cf18e469033a6390f47b79e5e70ceb
Felipe Trova <felipee.trova@gmail.com>
2020-03-27 22:32:54 -0300

    Add base page project + Service Api + simples styles

---
+++ pages/index.js
@@ -0,0 +1,84 @@
+import { useState, useEffect, useCallback } from "react";
+
+import * as GridStyle from "~/styles/Grid";
+
+import HeaderComponent from "~/components/Header/Header";
+import LoadingComponent from "~/components/Loading/Loading";
+import PokemonComponent from "~/components/CardPokemon/CardPokemon";
+
+import API from "~/Services/Api";
+
+const Index = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+  
+  const loadPokType = useCallback(async () => {
+    const idPokType = Math.floor(Math.random() * 19);
+
+    if(idPokType !== 0) {
+      try {
+        setLoading(true);
+        setErrorMsg("");
+        
+        const response = await API.get(`type/${idPokType}/`);
+        console.log(response.data.pokemon);
+        
+        setPokemon(response.data);
+  
+        if (response.data.length === 0) {
+          setErrorMsg("Nenhum Pokémon encontrado. Favor tentar novamente.");
+        }
+      } catch (error) {
+        setErrorMsg("Nenhum Pokémon encontrado. Favor tentar novamente.");
+      }
+    }
+
+    setLoading(false);
+  });
+
+  useEffect(() => {
+    loadPokType();
+  }, []);
+
+  return (
+    <>
+      {loading && <LoadingComponent />}
+
+      <HeaderComponent
+        link="/"
+        title={`Pokémon Type - ${pokemon.name}`}
+      />
+
+      <div className="mt-85px">
+        {(errorMsg || pokemon.length === 0) && (
+          <div className="flex align-itcenter cont-center h-75vh">
+            <p>{errorMsg}</p>
+          </div>
+        )}
+        
+        <GridStyle.Container>
+          <GridStyle.Row>
+            {pokemon.pokemon && pokemon.pokemon.length > 0 &&
+              pokemon.pokemon.map((item, index) => (
+                <GridStyle.Col
+                  key={index + 1}
+                  mobile={12}
+                  tablet={6}
+                  general={4}
+                >
+                  <PokemonComponent
+                    pokemonName={item.pokemon.name}
+                    pokemonUrl={item.pokemon.url}
+                  />
+                </GridStyle.Col>
+              ))
+            }
+          </GridStyle.Row>
+        </GridStyle.Container>
+      </div>
+    </>
+  );
+};
+
+export default Index;
